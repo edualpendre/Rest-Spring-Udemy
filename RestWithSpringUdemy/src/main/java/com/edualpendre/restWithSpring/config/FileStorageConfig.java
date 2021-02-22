a4ee23d33cd9c011e2e4475a78994e2f152939ae
Edu <edualpendre@gmail.com>
2021-02-22 16:47:18 -0300

    Continuous integration with Travis CI, Docker Hub, Amazon ECR and ECS

--- RestWithSpringUdemy/src/main/java/com/edualpendre/restWithSpring/config/FileStorageConfig.java
+++ RestWithSpringUdemy/src/main/java/com/edualpendre/restWithSpring/config/FileStorageConfig.java
@@ -1,10 +1,14 @@
 package com.edualpendre.restWithSpring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "file")
+//@ConfigurationProperties(prefix = "file")
+@Configuration
 public class FileStorageConfig {
 
+    @Value("${app.upload.dir:${user.home}}")
     private String uploadDir;
 
     public String getUploadDir() {
